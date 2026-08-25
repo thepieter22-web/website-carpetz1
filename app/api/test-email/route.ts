5ebfe6522512951e6653f323348f3f672e0af014
thepieter22-web <thepieter22@gmail.com>
2026-08-25 21:09:21 +0200

    Change email sender address in test-email route

--- app/api/test-email/route.ts
+++ app/api/test-email/route.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function GET() {
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "Carpetz <noreply@carpetz.be>",
       to: "info@carpetz.be",
       subject: "Carpetz Testmail",
       html: `
