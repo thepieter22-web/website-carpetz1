92046c138bcc9e94c3a5aa82fb6ab0f905559ae2
thepieter22-web <thepieter22@gmail.com>
2026-08-27 12:49:37 +0200

    Remove order button from price calculator
    Removed the button for placing an order in the price calculator.

--- components/mat-configurator/price-calculator.tsx
+++ components/mat-configurator/price-calculator.tsx
@@ -142,16 +142,7 @@ const finalTotal =
 
 
 
-<div className="pt-4">
-  <Button
-    className="w-full bg-[#C69C4D] hover:bg-[#B88D3C] text-white"
-   onClick={() => {
-  window.location.href = `/cart?type=${config.indoorSubtype}&width=${config.size.width}&height=${config.size.height}&quantity=${config.quantity}&total=${pricing.total.toFixed(2)}`;
-}}
-  >
-    Bestelling plaatsen
-  </Button>
-</div>
+
 
 </CardContent>
 </Card>
