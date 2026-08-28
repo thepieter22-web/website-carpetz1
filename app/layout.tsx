c878c5b6da21cd3ec1debd259a5fed5d78eb003f
thepieter22-web <thepieter22@gmail.com>
2026-08-28 12:30:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,8 @@ export const metadata: Metadata = {
   },
 }
 export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
   colorScheme: 'light',
   themeColor: '#f7f4ee',
 }
