badd8b10275fb10b0f816cf0acc431837be6905c
thepieter22-web <thepieter22@gmail.com>
2026-09-10 20:04:06 +0200

    Define PrintGrassColor interface and colors array
    Added an interface and a constant array for print grass colors.

--- lib/printgrass-colors.ts
+++ lib/printgrass-colors.ts
@@ -1,1 +1,58 @@
+export interface PrintGrassColor {
+  code: string;
+  name: string;
+}
+
+export const PRINTGRASS_COLORS: PrintGrassColor[] = [
+  { code: "600", name: "Wit" },
+  { code: "601", name: "Geel" },
+  { code: "602", name: "Goudgeel" },
+  { code: "603", name: "Oranjegeel" },
+  { code: "604", name: "Oranje" },
+  { code: "605", name: "Rood" },
+  { code: "606", name: "Bordeaux" },
+  { code: "607", name: "Fuchsia" },
+  { code: "608", name: "Roze" },
+  { code: "609", name: "Paars" },
+
+  { code: "610", name: "Lila" },
+  { code: "611", name: "Violet" },
+  { code: "612", name: "Lavendel" },
+  { code: "613", name: "Donkerblauw" },
+  { code: "614", name: "Lichtblauw" },
+  { code: "615", name: "Blauw" },
+  { code: "616", name: "Turquoise" },
+  { code: "617", name: "Petrol" },
+  { code: "618", name: "Mintgroen" },
+  { code: "619", name: "Bosgroen" },
+
+  { code: "620", name: "Lichtgroen" },
+  { code: "621", name: "Donkerpaars" },
+  { code: "622", name: "Taupe" },
+  { code: "623", name: "Marineblauw" },
+  { code: "624", name: "Grijsblauw" },
+  { code: "625", name: "Bruin" },
+  { code: "626", name: "Beige" },
+  { code: "627", name: "Donkerbruin" },
+  { code: "628", name: "Lichtgrijs" },
+  { code: "629", name: "Zilvergrijs" },
+
+  { code: "630", name: "Middengrijs" },
+  { code: "631", name: "Antraciet" },
+  { code: "632", name: "Zwart" },
+
+  { code: "901", name: "Grafiet" },
+  { code: "902", name: "Donkergrijs" },
+  { code: "903", name: "Diepzwart" },
+
+  { code: "904", name: "Crème" },
+  { code: "905", name: "Kastanjebruin" },
+  { code: "906", name: "Robijnrood" },
+  { code: "907", name: "Dennengroen" },
+  { code: "908", name: "Nachtblauw" },
+  { code: "909", name: "Donkerrood" },
+  { code: "910", name: "Gemêleerd Bruin" },
+  { code: "911", name: "Gemêleerd Grijs" },
+  { code: "912", name: "Koperbruin" },
+];
 
