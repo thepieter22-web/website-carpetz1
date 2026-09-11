3c0a77fabfd79e7400b015245a02c01b643a70dd
thepieter22-web <thepieter22@gmail.com>
2026-09-11 17:26:03 +0200

    Refactor PriceCalculator visibility for mobile and desktop

--- components/mat-configurator/index.tsx
+++ components/mat-configurator/index.tsx
@@ -823,10 +823,9 @@ const steps = [
           </div>
 
           {/* Price Calculator - Desktop */}
-<div className="hidden lg:block">
-  <div className="sticky top-24">
-    <PriceCalculator config={config} />
-  </div>
+<div className="lg:hidden mt-6">
+  <PriceCalculator config={config} />
+</div>
 </div>
         </div>
 
